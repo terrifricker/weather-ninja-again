039ced4b0048036f7de07709a2b3d045b1af8477
terrifricker <terrifricker@yahoo.com>
2021-05-26 10:44:10 -0700

    js: added little section to test getCity() with 'malibu'

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -23,7 +23,11 @@ const getCity = async (city) => {
     const response = await fetch(base + query);
     const data = await response.json();
 
-    console.log(data[0]);
+    return data[0];
 }
 
-getCity('manchester');
+getCity('malibu').then(data => {
+    return getWeather(data.Key);
+    }).then(data => {
+        console.log(data);
+    }).catch(err => { console.log(err); })

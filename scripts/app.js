01f981e4906ee9b5e4a272c2b646d0a95d8e57d0
terrifricker <terrifricker@yahoo.com>
2021-05-26 13:24:40 -0700

    remove <main> tag and replace with a <div class="card" to reflect the tutorial

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,5 @@
 const cityForm = document.querySelector('form');
-const card = document.querySelector("main");
+const card = document.querySelector(".card");
 const details = document.querySelector(".details");
 
 

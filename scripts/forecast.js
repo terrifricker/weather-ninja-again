735649a162d88889eabcf8e5f26ad85ed6c9d90e
terrifricker <terrifricker@yahoo.com>
2021-05-26 10:43:28 -0700

    js: add function getWeather()

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,5 +1,20 @@
 const key = 'SIOTsEz9ysEBcldZ8iAw1eHDwA4F3iAg';
 
+
+
+//get weather
+const getWeather = async (id) => {
+
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+}
+
+// get city
 const getCity = async (city) => {
 
     const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
